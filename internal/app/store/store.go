873000372f288f104e903115c7549143dd2f7399
Nikita Marakushev <63608270+NikitaMarakushev@users.noreply.github.com>
2022-10-22 23:43:43 +0300

    Store and dockerizing app

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -1,18 +1,37 @@
 package store
 
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
 type Store struct {
-	config *Config
+	config   *Config
+	database *sql.DB
 }
 
 func New(config *Config) *Store {
-	return &Config{
+	return &Store{
 		config: config,
 	}
 }
-func (s *Store) Open() error {
+func (store *Store) Open() error {
+	database, err := sql.Open("postgres", store.config.DatabaseURL)
+
+	if err != nil {
+		return err
+	}
+
+	if err := database.Ping(); err != nil {
+		return err
+	}
+
+	store.database = database
+
 	return nil
 }
 
-func (s *Store) Close() {
-
+func (store *Store) Close() {
+	store.database.Close()
 }
