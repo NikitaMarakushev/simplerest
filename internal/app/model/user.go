1069b101e6bcd37db625f58afa02d4bacea397d4
Nikita Marakushev <63608270+NikitaMarakushev@users.noreply.github.com>
2022-10-25 18:20:36 +0300

    encrypt string

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -1,7 +1,34 @@
 package model
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID                int
 	Email             string
+	Password          string
 	EncryptedPassword string
+}
+
+func (user *User) BeforeCreate() error {
+
+	if len(user.Password) > 0 {
+		enc, err := encryptString(user.Password)
+		if err != nil {
+			return err
+		}
+
+		user.EncryptedPassword = enc
+	}
+
+	return nil
+}
+
+func encryptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
