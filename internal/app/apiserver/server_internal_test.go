1e2e38300ec851f25370313f0085b15071d0736f
Nikita Marakushev <63608270+NikitaMarakushev@users.noreply.github.com>
2022-10-25 22:05:42 +0300

    TestServer_ServerHTTP

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"simplerest/internal/app/model"
 	"simplerest/internal/app/store/teststore"
 	"testing"
 )
@@ -13,9 +14,9 @@ import (
 func TestServer_ServerHTTPS(t *testing.T) {
 	s := newServer(teststore.New())
 	testCases := []struct {
-		name        string
-		payload     interface{}
-		exectedCode int
+		name         string
+		payload      interface{}
+		expectedCode int
 	}{
 		{
 			name: "valid",
@@ -23,7 +24,19 @@ func TestServer_ServerHTTPS(t *testing.T) {
 				"email":    "user@example.org",
 				"password": "password",
 			},
-			exectedCode: http.StatusCreated,
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid params",
+			payload: map[string]string{
+				"email": "invalid",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
 		},
 	}
 
@@ -34,7 +47,39 @@ func TestServer_ServerHTTPS(t *testing.T) {
 			json.NewEncoder(b).Encode(tc.payload)
 			req, _ := http.NewRequest(http.MethodPost, "/users", b)
 			s.ServerHTTP(rec, req)
-			assert.Equal(t, tc.exectedCode, rec.Code)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+}
+
+func TestServer_ServerHTTP(t *testing.T) {
+	u := model.TestUser(t)
+	store := teststore.New()
+	store.User().Create(u)
+	s := newServer(teststore.New())
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    u.Email,
+				"password": u.Password,
+			},
+			expectedCode: http.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/sessions", b)
+			s.ServerHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
 		})
 	}
 }
